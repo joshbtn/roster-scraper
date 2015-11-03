3e5a3e2d67cda2aaeb0f2636223f1d2270b7b23b
Josh Bennett <josh@j-ben.com>
2015-11-03 12:17:44 -0500

    Hashing out idea for roster config files

---
+++ config/rosters/nfl.js
@@ -0,0 +1,29 @@
+var nfl = {
+  "team": [
+    {
+      "name": "Eagles",
+      "url": "http://www.philadelphiaeagles.com/",
+      "roster": {
+        "url": "http://www.philadelphiaeagles.com/team/roster.html",
+        "selector" : {
+          "number": "table tbody tr td.col-jersey",
+          "name": "table tbody tr td.col-name",
+          "position": "table tbody tr td.col-position",
+          "weight": "table tbody tr td.col-weight",
+          "height": "table tbody tr td.col-height",
+          "age": "table tbody tr td.col-bd",
+          "experience": "table tbody tr td.col-exp",
+          "college": "table tbody tr td.col-college",
+          "squad" : function( $, currentValue, index, fullArray ) {
+            var currentRow = "table tbody tr:eq(" + index + ")",
+                $squadHeader = $(currentRow).closest('h2');
+            return  $squadHeader.text(),
+          }
+        },
+      },
+      "info": {
+        "url": "",
+      }
+    }
+  ]
+}
