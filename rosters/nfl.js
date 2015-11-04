ff61091064c2af6539ebe3fc66af444664a6f787
Josh Bennett <josh@j-ben.com>
2015-11-04 18:47:03 -0500

    Add feature to load roster modules from roster folder
    Main.js now loads roster module based on name and loops through teams.
    No real functionality yet.

--- rosters/nfl.js
+++ rosters/nfl.js
@@ -5,15 +5,16 @@ var nfl = {
       "url": "http://www.philadelphiaeagles.com/",
       "roster": {
         "url": "http://www.philadelphiaeagles.com/team/roster.html",
-        "selector" : {
-          "number": "table tbody tr td.col-jersey",
-          "name": "table tbody tr td.col-name",
-          "position": "table tbody tr td.col-position",
-          "weight": "table tbody tr td.col-weight",
-          "height": "table tbody tr td.col-height",
-          "age": "table tbody tr td.col-bd",
-          "experience": "table tbody tr td.col-exp",
-          "college": "table tbody tr td.col-college",
+        "document": "",
+        "dataSource" : {
+          "number" : "table tbody tr td.col-jersey",
+          "name" : "table tbody tr td.col-name",
+          "position" : "table tbody tr td.col-position",
+          "weight" : "table tbody tr td.col-weight",
+          "height" : "table tbody tr td.col-height",
+          "age" : "table tbody tr td.col-bd",
+          "experience" : "table tbody tr td.col-exp",
+          "college" : "table tbody tr td.col-college",
           "squad" : function($, currentValue, index, fullArray) {
             var currentRow = "table tbody tr:eq(" + index + ")",
                 $squadHeader = $(currentRow).closest('h2');
@@ -29,4 +30,4 @@ var nfl = {
   ]
 }
 
-module.export = nfl;
+module.exports = nfl;
