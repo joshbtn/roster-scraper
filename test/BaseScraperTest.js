4d3ab834a8af4b71055c8cd06dbec80a5193fd15
Josh Bennett <josh@j-ben.com>
2015-11-07 15:21:31 +0000

    Add test for nfl

--- test/BaseScraperTest.js
+++ test/BaseScraperTest.js
@@ -1,12 +1,6 @@
+var expect = require("chai").expect;
+
 describe("Scraper.Scrape", function() {
-  var Scraper = require('../lib/Scraper'),
-      nflRosters = require('../lib/rosters/nfl');
   
-  nflRosters.team[0].roster.document = "TODO, load static file";
-  var results = Scraper.scrape(nflRosters, {autoLoad: false});
-  
-  it("Should autoLoad document for NFL team Eagles.", function() {
-    expect(true).toBe(true);
-  });
 });
 
--- test/ScrapeTest.js
+++ test/ScrapeTest.js
@@ -1,9 +1,11 @@
+var expect = require("chai").expect;
+
 describe("Scrape", function() {
-  var scrape = require('lib/Scrape');  
+  var scrape = require('../lib/Scrape');  
   
   it("Srape should load the nfl scraper with no issues.", function() {
-    var nfl = Scrape("nfl", {autoLoad: false});
-    expect(nfl.autoLoad).toNotBe(null);
+    var nfl = scrape("nfl", {autoLoad: false});
+    expect(nfl.autoLoad).to.not.equal(null);
   });
   
 });
