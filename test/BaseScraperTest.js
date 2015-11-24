f4fffe58da828fd353f09e838115dbb2e1481182
Josh Bennett <josh@j-ben.com>
2015-11-24 19:38:58 +0000

    Add tests for base scraper

---
+++ test/BaseScraperTest.js
@@ -0,0 +1,19 @@
+var expect = require("chai").expect;
+
+describe("BaseScraper", function() {
+  
+  it("should return 3 rows of scraped data for test 1", function(){
+    
+    //expect(n).to.not.equal(null)
+  });
+  
+  beforeEach(function(){
+   
+  });
+  
+  afterEach(function(){
+    
+  });
+  
+});
+
