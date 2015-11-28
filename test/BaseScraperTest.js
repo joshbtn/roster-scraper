16238215248f09710ecf14326e933dac35a9123e
Josh Bennett <josh@j-ben.com>
2015-11-28 00:59:53 +0000

    start work on scrape

--- test/BaseScraperTest.js
+++ test/BaseScraperTest.js
@@ -63,3 +63,46 @@ describe("BaseScraper.getElementArray", function(){
 
   });
 });
+
+describe("baseScraper.scrape", function() {
+  it("should scrape the test eagles roster",function(){
+    var scrapper,
+        config = eaglesTest,
+        data;
+    
+      scrapper= new BaseScraper(config);
+      
+      data = scrapper.scrape();
+      
+      expect(false).to.equal(true)
+  });
+});
+
+var eaglesTest = {
+  "team": [
+    {
+      "name": "Eagles",
+      "uri": "",
+      "roster": {
+        "uri": "",
+        "document": fs.readFileSync(__dirname + "/assets/nfl_eagles.html"),
+        "dataSelector" : {
+          "number" : "table tbody tr td.col-jersey",
+          "name" : "table tbody tr td.col-name",
+          "position" : "table tbody tr td.col-position",
+          "weight" : "table tbody tr td.col-weight",
+          "height" : "table tbody tr td.col-height",
+          "age" : "table tbody tr td.col-bd",
+          "experience" : "table tbody tr td.col-exp",
+          "college" : "table tbody tr td.col-college",
+          "squad" : function($, currentElement, currentIndex, fullElementArray) {
+            var currentRow = "table tbody tr:eq(" + index + ")",
+                $squadHeader = $(currentRow).closest('h2');
+
+            return  $squadHeader.text();
+          }
+        }
+      }
+    }
+  ]
+}
