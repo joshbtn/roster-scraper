cb7c3a60fe3e235eac2b4af39afb80f67fb262cd
Josh Bennett <josh@j-ben.com>
2015-12-29 18:22:21 +0000

    Start work on output csv, added Table object for easy row counts

--- test/OutputCsvTest.js
+++ test/OutputCsvTest.js
@@ -1,22 +1,27 @@
 'use strict';
-
+/* global context */
 const expect = require("chai").expect;
 const OutputCsv = require(__dirname + "/../lib/OutputCsv.js");
 const Scraper = require(__dirname + "/../lib/Scraper.js");
-let eaglesTest = require(__dirname + "/helpers/EaglesTestConfig");
 
 describe("OutputCsv", function() {
   
-  let scraper = new Scraper(eaglesTest);
+  let scraper = new Scraper();
   
   describe("#get()", function(){
       
-      context("with a scraper set", function(){
+      context("basic set data test", function(){
       
-        var outputCsv = new OutputCsv(scraper);
+        it('should return "1,2,3\\n4,5,6"', function(){
+          scraper.setData([[1,4],[2,5],[3,6]]);
+          let outputCsv = new OutputCsv(scraper);
+          let csvString = outputCsv.get();
         
-      })
+          expect(csvString).to.be.equal("1,2,3\\n4,5,6");
+        });
+        
+      });
       
-  })
+  });
  
 });
