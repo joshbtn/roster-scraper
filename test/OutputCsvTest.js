0f4445b63d5f31c4c84d2b4eca72227297a7edbb
Josh Bennett <josh@j-ben.com>
2015-12-29 21:39:52 +0000

    all output string tests are passing

--- test/OutputCsvTest.js
+++ test/OutputCsvTest.js
@@ -3,6 +3,7 @@
 const expect = require("chai").expect;
 const OutputCsv = require(__dirname + "/../lib/OutputCsv.js");
 const Scraper = require(__dirname + "/../lib/Scraper.js");
+const Table = require(__dirname + "/../lib/Table.js");
 
 describe("OutputCsv", function() {
   
@@ -11,13 +12,15 @@ describe("OutputCsv", function() {
   describe("#get()", function(){
       
       context("basic set data test", function(){
-      
-        it('should return "1,2,3\\n4,5,6"', function(){
-          scraper.setData([[1,4],[2,5],[3,6]]);
+        
+        it('should return expected CsvString', function(){
+          let table = new Table();
+          table.push([1,4,7],[2,5,8],[3,6,9])
+          scraper.setData(table);
           let outputCsv = new OutputCsv(scraper);
           let csvString = outputCsv.get();
         
-          expect(csvString).to.be.equal("1,2,3\\n4,5,6");
+          expect(csvString).to.be.equal("1,2,3\n4,5,6\n7,8,9");
         });
         
       });
