b29d20f033ac3af876602e92ff00ff2ebf0d386b
Josh Bennett <josh@j-ben.com>
2016-08-17 16:01:15 +0000

    Latest work

--- rosters/NFL.js
+++ rosters/NFL.js
@@ -1,13 +1,11 @@
 'use strict';
+
+const _ = require('underscore');
 const crypto = require('crypto');
 
 const secret = '';
 
-var nfl = [{
-    "name": "Eagles",
-    "uri": "http://www.philadelphiaeagles.com/team/roster.html",
-    "document": "",
-    "dataSelector": {
+const dataSelector = {
       "id": function($, currentRowIndex, fullElementArray) {
 
         var getTextNodesIn = function(el) {
@@ -53,64 +51,39 @@ var nfl = [{
 
         return squad;
       }
+};
 
-    }
+function getTeamSelector(teamName) {
+   var cloned = _.clone(dataSelector);
+   cloned.team = function(){ return teamName; }
+}
+
+var nfl = [
+  {
+    "name": "Bengals",
+    "uri": "http://www.bengals.com/team/roster.html",
+    "dataSelector": getTeamSelector("Bengals"),
   },
-  
+  {
+    "name": "Browns",
+    "uri": "http://www.clevelandbrowns.com/team/roster.html",
+    "dataSelector": getTeamSelector("Browns"),
+  },
+  {
+    "name": "Eagles",
+    "uri": "http://www.philadelphiaeagles.com/team/roster.html",
+    "dataSelector": getTeamSelector("Eagles")
+  },
+  {
+    "name": "Steelers",
+    "uri": "http://www.steelers.com/team/roster.html",
+    "dataSelector": getTeamSelector("Steelers")
+  },
   {
     "name": "Ravens",
     "uri": "http://www.baltimoreravens.com/team/roster.html",
-    "document": "",
-    "dataSelector": {
-      "id": function($, currentRowIndex, fullElementArray) {
-
-        var getTextNodesIn = function(el) {
-          return el.find(":not(iframe)").addBack().contents().filter(function() {
-            return this.nodeType == 3;
-          });
-        };
-
-        var rows = $("table tbody tr"),
-          currentRow = rows.eq(currentRowIndex), //:eq(" + currentRowIndex + ")",
-          name = getTextNodesIn($(currentRow).find("td.col-name a span")).text(),
-          position = getTextNodesIn($(currentRow).find("td.col-position")).text(),
-          first = name.split(',')[1].trim(),
-          last = name.split(',')[0].trim(),
-          to_hash = position + first + last;
-
-        const hash = crypto.createHmac('sha1', secret)
-          .update(to_hash)
-          .digest('hex');
-
-        return hash;
-      },
-      "team": function(){return "Ravens"},
-      "number": "table tbody tr td.col-jersey",
-      "name": "table tbody tr td.col-name a span",//One of these may be broken, which needs a test case.
-      "position": "table tbody tr td.col-position",
-      "weight": "table tbody tr td.col-weight",
-      "height": "table tbody tr td.col-height",
-      "age": "table tbody tr td.col-bd",
-      "experience": "table tbody tr td.col-exp",
-      "college": "table tbody tr td.col-college",
-      "squad": function($, currentRowIndex, fullElementArray) {
-        var getTextNodesIn = function(el) {
-          return el.find(":not(iframe)").addBack().contents().filter(function() {
-            return this.nodeType == 3;
-          });
-        };
-        var rows = $("table tbody tr"),
-          currentRow = rows.eq(currentRowIndex), //:eq(" + currentRowIndex + ")",
-          table = $(currentRow).closest('table'),
-          squadHeader = $(table).prevAll('.mod-title-nobackground').find('h2'),
-          squad = getTextNodesIn(squadHeader).text();
-
-        return squad;
-      }
-
-    }
+    "dataSelector": getTeamSelector("Ravens")
   }
-
 ]
 
 module.exports = nfl;
